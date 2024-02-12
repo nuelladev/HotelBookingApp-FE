199a2b4120f0f57f784f4d485f2e4fc14294b45a
nuelladev <emmanuellaokafor42@yahoo.com>
2024-02-12 12:59:16 +0100

    feature: added the completed add room feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
