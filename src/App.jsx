1dcb6a22eae75f4a479f9e08c67424bbaa6f9fca
nuelladev <emmanuellaokafor42@yahoo.com>
2024-02-12 12:30:38 +0100

    bug-fix: fixed css issues

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
+import AddRoom from  "../src/components/room/AddRoom"
+
+function App() {
+
+    return <>
+    <AddRoom />
+    </>
+
+}
+export default App
